9ad45d873524fe19011a4b0791dc4b0714229b71
solanokarl048-boop <solanokarl048@gmail.com>
2026-09-18 20:02:54 +0800

    Delete aml-psdk directory

--- aml-psdk/plugin.h
+++
@@ -1,8 +0,0 @@
-#ifndef __AML_PSDK_GAMEPLUGINROOT_H
-#define __AML_PSDK_GAMEPLUGINROOT_H
-
-#define BASE_GAME_LIBRARY "libGTASA.so"
-#define GTASA
-#include <aml-psdk/sdk_base.h>
-
-#endif // __AML_PSDK_GAMEPLUGINROOT_H
